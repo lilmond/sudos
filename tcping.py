202bb710b0fc7e1f924fb6a17b2bc691a2c0894e
lilmond <54730301+lilmond@users.noreply.github.com>
2021-06-27 18:07:52 +0000

    Update tcping.py

--- tcping.py
+++ tcping.py
@@ -12,7 +12,7 @@ elif sys.platform == "win32":
 parser = argparse.ArgumentParser()
 parser.add_argument("host", nargs="?", help="Host to perform TCP ping on")
 parser.add_argument("-p", "--port", default=80, type=int, help="Port of host to perforn TCP ping on")
-parser.add_argument("-t", "--timeout", default=5, type=int, help="TCP connection timeout value")
+parser.add_argument("-t", "--timeout", default=3, type=int, help="TCP connection timeout value")
 
 args = parser.parse_args()
 
@@ -24,30 +24,30 @@ if not args.host:
 host = args.host
 port = args.port
 timeout = args.timeout
-socket.setdefaulttimeout(1)
+socket.setdefaulttimeout(timeout)
 sequence = 1
 
-ip = socket.gethostbyname(host)
+try:
+    ip = socket.gethostbyname(host)
+except Exception as e:
+    print(f"Unable to resolve domain name: {e}")
+    sys.exit()
 
-if host != ip:
-    domain_ip_string = f"(\u001b[32;1m{ip}\u001b[0;0m)"
-    host = ip
-else:
-    domain_ip_string = ""
-
-
-print(f"Initializing TCP ping to \u001b[32;1m{host}\u001b[0;0m{domain_ip_string}:\u001b[32;1m{port}\u001b[0;0m...\r\n")
+print(f"Initializing TCP ping to \u001b[32;1m{host}\u001b[0;0m:\u001b[32;1m{port}\u001b[0;0m...")
+print(f"")
 try:
     while True:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         time1 = time.time()
-        sock.connect((host, port))
-        print(f"Host=\u001b[32;1m{host}\u001b[0;0m Port=\u001b[32;1m{port}\u001b[0;0m Time=\u001b[32;1m{1000 * (time.time() - time1):.2f}ms\u001b[0;0m Sequence=\u001b[32;1m{sequence}\u001b[0;0m")
-        sock.close()
+        sock.connect((ip, port))
+        time2 = time.time()
+        ping = (time2 - time1) * 1000
+        print(f"Host=\u001b[32;1m{ip}\u001b[0;0m Port=\u001b[32;1m{port}\u001b[0;0m Time=\u001b[32;1m{ping:.2f}ms\u001b[0;0m Sequence=\u001b[32;1m{sequence}\u001b[0;0m")
         sequence += 1
+        sock.close()
         time.sleep(1)
+except Exception as e:
+    print(f"\u001b[31;1mError\u001b[0;0m: {e}")
+    pass
 except KeyboardInterrupt:
     sys.exit()
-except Exception as e:
-    print(f"Error: {e}")
-    pass
